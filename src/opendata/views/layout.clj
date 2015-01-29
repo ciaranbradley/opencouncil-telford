11c2bb72f4353886f38e14f602e588315295ce3e
Ciaran Bradley <ciaran.p.bradley@gmail.com>
2015-01-29 08:11:07 +0000

    Menu items in layout

--- src/opendata/views/layout.clj
+++ src/opendata/views/layout.clj
@@ -4,13 +4,13 @@
 
 (defn menu []
   [:div.menu
-   (link-to (str "#/service-delivery-area") "Delivery Area")
-   (link-to (str "#/service-delivery-team") "Delivery Team")
-   (link-to (str "#/cost-c") "Cost C")
-   (link-to (str "#/expenditure-group") "Expenditure Group")
-   (link-to (str "#/account") "Account")
-   (link-to (str "#/supplier-name") "Supplier Name")
-   (link-to (str "#/transaction-date") "Transaction Date")])
+   (link-to (str "#/service-delivery-area") "Delivery Area ")
+   (link-to (str "#/service-delivery-team") "Delivery Team ")
+   (link-to (str "#/cost-c") "Cost C ")
+   (link-to (str "#/expenditure-group") "Expenditure Group ")
+   (link-to (str "#/account") "Account ")
+   (link-to (str "#/supplier-name") "Supplier Name ")
+   (link-to (str "#/transaction-date") "Transaction Date ")])
 
 (defn common [& body]
   (html5
