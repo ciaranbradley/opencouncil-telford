e578f56731ab02d8fd931d45284ea6512af0606a
Ciaran Bradley <ciaran.p.bradley@gmail.com>
2015-01-31 17:53:41 +0000

    Need some tests. Perhaps push all the api routes through a test macro?

--- test/opendata/test/handler.clj
+++ test/opendata/test/handler.clj
@@ -1,8 +1,13 @@
 (ns opendata.test.handler
-  (:require [opendata.routes.home :as home])
-  (:use clojure.test
-        ring.mock.request
-        opendata.handler))
+  (:require [opendata.routes.home :as home]
+            [clojure.test :refer :all]
+            [ring.mock.request :as mock]
+            [opendata.handler :refer :all]
+            [opendata.routes.apiv1.transparency-report
+             :refer [reports
+                     exp-over-100-format
+                     get-report-amounts]]
+  (:use midje.sweet))
 
 (def testdata {"" "",
                "Amount" "150.00",
@@ -15,6 +20,14 @@
                "Service Delivery Area(T)" "Adult Social Services"})
 
 
+
+
+(facts "GET /api/v1/trasparency-report/2014/january/supplier-name"
+  (fact "Test GET"
+        (let [response (app (mock/request :get "/api/v1/transparency-report/2014/january/supplier-name"))]
+          (:status response) => 200)))
+
+
 (deftest test-amount-by-key
   (testing "amount-by-key returns correct data"
     (is (= (home/amount-by-key :supplier-name testdata)
@@ -24,4 +37,8 @@
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= (:status response) 404))))
+
+  (testing "api route exists"
+    (let [response (app (request :get "/api/v1/trasparency-report/2014/january/supplier-name"))]
+      (is (= (:status response) 200)))))
