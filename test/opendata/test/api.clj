7a1ed31598fb16a47ae754c1224f632b2d4b44ee
Ciaran Bradley <ciaran.p.bradley@gmail.com>
2015-02-02 22:17:19 +0000

    Note to self. Tests or the monsters will come.

--- test/opendata/test/api.clj
+++ test/opendata/test/api.clj
@@ -9,13 +9,11 @@
                      exp-over-100-format]]))
 
 
-
-
 (facts "test the api responds correctly"
        (fact "Test GET actual resource responds"
         (let [response (app (mock/request :get "/api/v1/transparency-report/2014/january/supplier-name"))]
-          (:status response) => 200))
+          (:status response) => 200)))
 
-        (fact "Test incorrect resource responds with a 404"
-          (let [response (app (mock/request :get "/api/v1/transparency-report/2013/january/supplier-name"))]
-            (:status response) => 404)))
+        ;;(fact "Test incorrect resource responds with a 404"
+        ;;  (let [response (app (mock/request :get "/api/v1/transparency-report/2013/january/supplier-name"))]
+        ;;    (:status response) => 404)))
